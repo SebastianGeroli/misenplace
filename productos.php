f2b42b567342d5063a7bcdca7661a6b615888725
Sebastián Geroli <sebastiangeroli@outlook.com>
2020-08-01 18:05:05 -0300

    Actualizando css con flexbox, y añadido layout de la pagina productos

--- productos.php
+++ productos.php
@@ -1,8 +1,56 @@
-<?php 
+<?php
 include 'head.php';
 include 'navBar.php';
 ?>
-
-<?php 
-include 'footer.php'
+<div class="barraproductos">
+    <div class="contenedor">
+        <nav class="navcategorias">
+            <a href="#">Pastas</a>
+            <a href="#">Empanadas</a>
+            <a href="#">Hamburguesas</a>
+        </nav><!--.navcategorias-->
+    </div>
+    <!--.contenedor-->
+</div>
+<!--.barraproductos-->
+    <div class="contenedor">
+        <ul class="listaProductos">
+            <li class="producto">
+                <div class="contenedorProducto">
+                    <h5>Nombre Producto</h5>
+                    <p>Descripcion Producto</p>
+                </div>
+            </li>
+            <li class="producto">
+                <div class="contenedorProducto">
+                    <h5>Nombre Producto</h5>
+                    <p>Descripcion Producto2</p>
+                </div>
+            </li>
+            <li class="producto">
+                <div class="contenedorProducto">
+                    <h5>Nombre Producto</h5>
+                    <p>Descripcion Producto 2</p>
+                </div>
+            </li>
+            <li class="producto">
+                <div class="contenedorProducto">
+                    <h5>Nombre Producto</h5>
+                    <p>Descripcion Producto 2</p>
+                </div>
+            </li>   <li class="producto">
+                <div class="contenedorProducto">
+                    <h5>Nombre Producto</h5>
+                    <p>Descripcion Producto 2</p>
+                </div>
+            </li>   <li class="producto">
+                <div class="contenedorProducto">
+                    <h5>Nombre Producto</h5>
+                    <p>Descripcion Producto 2</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+<?php
+include 'footer.php';
 ?>
