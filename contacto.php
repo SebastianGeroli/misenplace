fca6dcfc9abf4f879d6b543a2b0dc4d5b9122130
Sebastián Geroli <sebastiangeroli@outlook.com>
2020-09-09 15:33:40 -0300

    form contacto y root colors

--- contacto.php
+++ contacto.php
@@ -1,8 +1,41 @@
-<?php 
+<?php
 include 'head.php';
 include 'navBar.php';
 ?>
+<div class="contenedor">
+    <form class="contacto">
+        <div class="formBackground clearfix">
 
-<?php 
-include 'footer.php'
+            <div class="formContainer">
+                <label for="fname">Nombre:</label><br>
+                <input type="text" id="fname" name="fname"><br>
+            </div><!--.formContainer-->
+
+            <div class="formContainer">
+                <Label for="lname">Apellido:</Label><br>
+                <input type="text" id="lname" name="lname"><br>
+            </div><!--.formContainer-->
+            
+            <div class="formContainer">
+                <label for="email">Email:</label><br>
+                <input type="text" id="email" name="email"><br>
+            </div><!--.formContainer-->
+            
+            <div class="formContainer">
+                <label for="phoneNumber">Telefono (Opcional):</label><br>
+                <input type="text" name="phoneNumber" id="phoneNumber"><br>
+            </div><!--.formContainer-->
+            
+            <div class="formContainer">
+                <label for="message">Mensaje:</label><br>
+                <textarea id="message" name="message"></textarea>
+            </div><!--.formContainer-->
+
+            <input type="submit" value="Enviar" class="submit-btn">
+            
+        </div><!--.formBackground-->
+    </form><!--.contacto-->
+</div>
+<?php
+include 'footer.php';
 ?>
--- quienesSomos.php
+++ quienesSomos.php
@@ -2,7 +2,15 @@
 include 'head.php';
 include 'navBar.php';
 ?>
+<div class="left-column">
 
+</div><!--.left-column-->
+<div class="mid-column">
+
+</div><!--.mid-column-->
+<div class="right-column">
+
+</div><!--.right-column-->
 <?php 
-include 'footer.php'
+include 'footer.php';
 ?>
